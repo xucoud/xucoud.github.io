c03cb9aca1c3072d62e3c8292727814e4f9f7eae
徐文杰 <281658285@qq.com>
2021-05-19 22:35:44 +0800

    Site updated: 2021-05-19 22:35:44

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
+function colortag(){
+$("ul.tag-list li").each(function(){
+      let random = Math.floor(Math.random()*5+1);
+      if(random == 2){
+          $(this).find('a').addClass('tagred');
+      } else if ( random == 3 ){
+          $(this).find('a').addClass('tagyellow');
+      } else if ( random == 4 ){
+          $(this).find('a').addClass('tagblue');
+      } else if ( random == 5 ){
+          $(this).find('a').addClass('taggreen');
+      } else if ( random == 2 ){
+          $(this).find('a').addClass('tagpurple');
+      } else if ( random == 1 ){
+          $(this).find('a').addClass('taggrown');
+      }
+  
+});}
+colortag();
+
+
 // 函数防抖 (只执行最后一次点击)
 var Debounce = (fn, t) => {
   const delay = t || 25;
